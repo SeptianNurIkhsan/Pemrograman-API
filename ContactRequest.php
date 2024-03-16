92d541d361dc80a278e0deb58e3d203d67e53fe4
22091397049 Septian Nur <114415096+SeptianNurIkhsan@users.noreply.github.com>
2024-03-16 19:59:49 +0700

    Request

---
+++ ContactRequest.php
@@ -0,0 +1,24 @@
+<?php
+// File: app/Http/Requests/ContactRequest.php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'required|string'
+        ];
+    }
+}
